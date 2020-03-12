7bb319c416d2e8aa5abf35ab612c9a8f036c478c
Lady RaeGun <raegun@gammarae.design>
2020-03-12 11:37:52 -0300

    Adding commenting

--- personalitty.sh
+++ personalitty.sh
@@ -12,6 +12,8 @@ export PTTYMOOD='PTTYCOOL';
 # export the personality for use in PS1, etc...
 if [[ -z "$PTTY" ]]; then export PTTY='( •_•)'; fi
 
+# converts the current mood to a face, using the 
+# current strength of that mood to pick an appropriate face
 function ptty_mood_to_face() {
     moodcountmedian=$(expr $MOODCOUNT / 2)
     moodval=$(printenv $1)
@@ -44,12 +46,16 @@ function ptty_mood_to_face() {
     esac
 }
 
-
+# this function possibly didn't need to be a function
+# holdover of a previous attempt. But breaking it off
+# like this could make things easier later for new functionality
 function ptty_rebuffer() {
     pttytmprc=$?;
     export PTTYCURRC=$pttytmprc;
 }
 
+# we don't really want the numbers to go up endlessly, so as one
+# mood gets a point, the other moods will lose one.
 function ptty_normalize() {
     for m in $(echo $PTTYMOODS); do
         if [[ ("$m" != "$1") && ($(printenv ${m}) -gt 0) ]]; then 
@@ -58,20 +64,26 @@ function ptty_normalize() {
     done;
 }
 
+# uses the return code to generate mood and set
+# the PTTY environment variable with a face
 function ptty_eval_current_mood {
     ptty_rebuffer;
     pttyemoteval=$(ptty_rc2emotes "$1")
     if [[ "${pttyemoteval}" -lt "$MOODCOUNT" ]]; then let "${pttyemoteval}++"; fi;
-    # set "$pttyemoteval"=$(expr $(echo "$pttyemoteval") + 1);
+    
+    # this creates a list of the moods and their scores and inverse 
+    # sorts them to get the dominant one. The sed removes the score
+    # so the mood name can be used.
     export PTTYMOOD=$(for v in $(echo $PTTYMOODS); do echo "`printenv $v`$v"; done | sort --reverse | head -1 | sed 's/[0-9]//g')
-    #echo ADD "$PTTYMOOD"=$(expr "$(printenv $PTTYMOOD) + 1");
-    #let "${PTTYMOOD}++"
+    
+    # normalize the values to make sure the scores don't 
+    # climb endlessly
     ptty_normalize "${pttyemoteval}";
     export PTTY=$(ptty_mood_to_face "$PTTYMOOD");
 
 }
 
-
+# associates a mood to a return code
 function ptty_rc2emotes() {
     case "$1" in 
      "0") # cool
@@ -101,6 +113,7 @@ function ptty_rc2emotes() {
     esac
 }
 
+# this should be called by the shell RC before every new command.
 function ptty_eval() {
     ptty_eval_current_mood $?;
 }
